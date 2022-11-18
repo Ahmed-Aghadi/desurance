d431ceae48a460a41f41acabc6c01c10896993ce
ahmedaghadi <ahmedaghadi123@gmail.com>
2022-11-19 02:23:46 +0530

    next.config updated

--- client/my-app/next.config.js
+++ client/my-app/next.config.js
@@ -4,4 +4,34 @@ const nextConfig = {
     swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://desurance-33c740.spheron.app",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ]
+    },
+    ...nextConfig,
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+    trailingSlash: true,
+}
